f70cb3e0aa4719761ee43b745c38fe7037c8405a
yxhong-tw <yxhong.tw@gmail.com>
2025-08-16 20:29:47 +0800

    chore: Add commands to prune model with different pruning ratios.

--- run.sh
+++ run.sh
@@ -69,3 +69,45 @@ python3 hf_prune.py --pruning_ratio 0.2 \
     --seed 48763 \
     --iterative_steps 1 \
     --calibration_data wikitext2 \
+
+# Compressed Llama-2-7b with Wikitext2 dataset and pruning ratio 0.4.
+python3 hf_prune.py --pruning_ratio 0.4 \
+    --block_wise \
+    --block_mlp_layer_start 0 --block_mlp_layer_end 32 \
+    --block_attention_layer_start 0 --block_attention_layer_end 0 \
+    --pruner_type taylor \
+    --device cpu  --eval_device cuda:2 \
+    --save_ckpt_log_name wikitext2_0d4 \
+    --base_model /workspace/models/Llama-2-7b-hf \
+    --save_model \
+    --seed 48763 \
+    --iterative_steps 1 \
+    --calibration_data wikitext2 \
+
+# Compressed Llama-2-7b with Wikitext2 dataset and pruning ratio 0.6.
+python3 hf_prune.py --pruning_ratio 0.6 \
+    --block_wise \
+    --block_mlp_layer_start 0 --block_mlp_layer_end 32 \
+    --block_attention_layer_start 0 --block_attention_layer_end 0 \
+    --pruner_type taylor \
+    --device cpu  --eval_device cuda:2 \
+    --save_ckpt_log_name wikitext2_0d6 \
+    --base_model /workspace/models/Llama-2-7b-hf \
+    --save_model \
+    --seed 48763 \
+    --iterative_steps 1 \
+    --calibration_data wikitext2 \
+
+# Compressed Llama-2-7b with Wikitext2 dataset and pruning ratio 0.8.
+python3 hf_prune.py --pruning_ratio 0.8 \
+    --block_wise \
+    --block_mlp_layer_start 0 --block_mlp_layer_end 32 \
+    --block_attention_layer_start 0 --block_attention_layer_end 0 \
+    --pruner_type taylor \
+    --device cpu  --eval_device cuda:2 \
+    --save_ckpt_log_name wikitext2_0d8 \
+    --base_model /workspace/models/Llama-2-7b-hf \
+    --save_model \
+    --seed 48763 \
+    --iterative_steps 1 \
+    --calibration_data wikitext2 \
